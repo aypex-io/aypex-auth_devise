d8f7df85842e8775e67187ca11ec4173ea82becc
Matthew Kennedy <m.kennedy@me.com>
2023-06-19 17:04:30 +0100

    Fix accounts pages.

--- app/controllers/concerns/aypex/auth_devise/base.rb
+++ app/controllers/concerns/aypex/auth_devise/base.rb
@@ -28,7 +28,7 @@ module Aypex
         # Strange behavior with Rails Engines and router_name.
         def signed_in_root_path(resource_or_scope)
           scope = Devise::Mapping.find_scope!(resource_or_scope)
-          router_name = Devise.mappings[scope].router_name
+          router_name = nil # Devise.mappings[scope].router_name
           home_path = "#{scope}_root_path"
 
           context = router_name ? send(router_name) : self
